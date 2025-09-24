0ddb9ce7da9b48963c877a1169e619b477e2de7a
tauheedbuttt <tauheedbutt@gmail.com>
2025-09-24 17:48:03 +0300

    3.8*: Phonebook backend step 8

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.json());
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/info", (req, res) => {
   return res.send(`
