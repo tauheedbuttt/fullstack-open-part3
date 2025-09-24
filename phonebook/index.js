52087f819203529f1c0d566ba68c442c58a12a36
tauheedbuttt <tauheedbutt@gmail.com>
2025-09-24 17:23:01 +0300

    feat(phonebook): 3.1

---
+++ phonebook/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const PORT = 3001;
+const app = express();
+
+const persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  return res.status(200).json(persons);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
