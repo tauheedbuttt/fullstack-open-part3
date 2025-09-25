9e50b6ff30ae2273c702ee574da0ef3b91ba5c72
tauheedbuttt <tauheedbutt@gmail.com>
2025-09-25 09:53:31 +0300

    feat(phonebook): frontend-build

--- phonebook/api/index.js
+++ phonebook/api/index.js
@@ -2,7 +2,7 @@ const app = require("../app");
 // const { createServer } = require("../helpers/socket.helper");
 
 //Use system configuration for port or use 3000 by default.
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
